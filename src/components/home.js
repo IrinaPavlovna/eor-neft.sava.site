99295ec0b8b00bc2cf54beb2dc03688597cc0076
YURII SILIN <try2scumme@gmail.com>
2022-11-30 01:25:27 +0300

    1

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Introduction from './Introduction'
 import Tabs from './tabs'
+import './home.scss'
+
+import main1PNG from '../img/main1.webp'
 function home() {
   return (
-    <div>
-    <h1 style={{ color: "", textAlign: "center", margin: "60px" }}>
-      Электронный образовательный ресурс
-    </h1>
-    <Introduction/>
-    <Tabs/>
-    
+    <div className="main">
+      <div className="main-content">
+        <h1>
+          Электронный образовательный ресурс
+        </h1>
+        <img src={main1PNG} alt="" className="main-img" />
+        <Introduction />
+        <Tabs />
+
+      </div>
     </div>
   );
 }
--- src/components/hydrotreating/hydrotreatingSwiperSchema.js
+++ src/components/hydrotreating/hydrotreatingSwiperSchema.js
@@ -1,12 +1,12 @@
 import React from "react";
-import IMG1 from "../../img/hydrotreating/schema/1.jpg";
-import IMG2 from "../../img/hydrotreating/schema/2.jpg";
-import IMG3 from "../../img/hydrotreating/schema/3.jpg";
-import IMG4 from "../../img/hydrotreating/schema/4.jpg";
+import IMG1 from "../../img/schema/1.jpg";
+import IMG2 from "../../img/schema/2.jpg";
+import IMG3 from "../../img/schema/3.jpg";
+import IMG4 from "../../img/schema/4.jpg";
 
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination  } from "swiper";
+import { Pagination } from "swiper";
 
 import "swiper/scss";
 import "swiper/scss/pagination";
@@ -14,22 +14,22 @@ import "swiper/scss/pagination";
 function hydrotreatingSwiper() {
   return (
     <Swiper
-    style={{ height:'74vh'}}
+      style={{ height: '74vh' }}
       pagination={true}
       modules={[Pagination]}
       className="mySwiper"
     >
       <SwiperSlide >
-        <img style={{  maxWidth:'fit-content', maxHeight:'100%'}} src={IMG1} alt="" />
+        <img style={{ maxWidth: 'fit-content', maxHeight: '100%' }} src={IMG1} alt="" />
       </SwiperSlide>
       <SwiperSlide>
-        <img style={{  maxWidth:'fit-content', maxHeight:'100%'}} src={IMG2} alt="" />
+        <img style={{ maxWidth: 'fit-content', maxHeight: '100%' }} src={IMG2} alt="" />
       </SwiperSlide>
       <SwiperSlide>
-        <img style={{  maxWidth:'fit-content', maxHeight:'100%'}} src={IMG3} alt="" />
+        <img style={{ maxWidth: 'fit-content', maxHeight: '100%' }} src={IMG3} alt="" />
       </SwiperSlide>
       <SwiperSlide>
-        <img  style={{  maxWidth:'fit-content', maxHeight:'100%'}} src={IMG4} alt="" />
+        <img style={{ maxWidth: 'fit-content', maxHeight: '100%' }} src={IMG4} alt="" />
       </SwiperSlide>
     </Swiper>
   );
