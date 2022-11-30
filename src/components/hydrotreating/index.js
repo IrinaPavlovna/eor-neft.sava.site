9d86bcee1ae799bac4a020faa7739622fd4b2913
AlexSavr <YuriAndreev11ch@gmail.com>
2022-11-30 10:37:12 +0300

    1

--- src/components/hydrotreating/index.js
+++ src/components/hydrotreating/index.js
@@ -17,10 +17,10 @@ import {
 function Hydrotreating() {
   return (
     <div className="hydrotreating">
-      <h1>Гидроочистка №2</h1>
 
       <div className="hydrotreating__content">
-        <HydrotreatingSwiper />
+        <h1>Гидроочистка №2</h1>
+
         <Accordion allowToggle width="100%">
           <AccordionItem>
             <h2>
@@ -31,7 +31,7 @@ function Hydrotreating() {
             </h2>
             <AccordionPanel pb={4}>
               <iframe
-                title='1'
+                title="1"
                 style={{ width: "100%", height: "324px" }}
                 src="https://drive.google.com/file/d/1ZKewRhnRg2wSZ6Si7t_iljP48jL37f7X/preview"
               />
@@ -91,7 +91,14 @@ function Hydrotreating() {
             </h2>
             <AccordionPanel pb={4}>
               <div className="hydrotreating__modal">
-                Исходный материал <a href="https://www.youtube.com/channel/UCa_ybOhOWMpVmDVohW_9LGg" target="_blank" rel="noreferrer">ПроНПЗ</a>
+                Исходный материал
+                <a
+                  href="https://www.youtube.com/channel/UCa_ybOhOWMpVmDVohW_9LGg"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  ПроНПЗ
+                </a>
                 <ModalComponent
                   name="Ректификационная колонна"
                   href="https://www.youtube.com/embed/ODaN3yi1wl8"
@@ -144,14 +151,53 @@ function Hydrotreating() {
             </h2>
             <AccordionPanel pb={4}>
               <div className="hydrotreating__modal">
-                <ModalComponent name='Правда или ложь' href='https://wordwall.net/ru/embed/ae746e14bc5b495297779fb7e866e157?themeId=48&templateId=35%22%20width=%22500%22%20height=%22380%22%20frameborder=%220%22%20allowfullscreen' />
-                <ModalComponent name='Найди слова' href='https://wordwall.net/ru/embed/ff041e58354c4dff975a284f728e88bc?themeId=44&templateId=10%22%20width=%22500%22%20height=%22380%22%20frameborder=%220%22%20allowfullscreen' />
-                <ModalComponent name='Назначение блоков' href='https://learningapps.org/watch?v=p2ts0hr4t21' />
-                <a href="https://onlinetestpad.com/2wyebagof4os6" target="_blank" rel="noreferrer"><Button>Кроссворд</Button></a>
-                <a href="https://onlinetestpad.com/fbptycgep2bby" target="_blank" rel="noreferrer"><Button>Тест "Реакторный блок"</Button></a>
-                <a href="https://onlinetestpad.com/5yuhaqbv6pkpq" target="_blank" rel="noreferrer"><Button>Тест "Блок стабилизации"</Button></a>
-                <a href="https://onlinetestpad.com/icdxqfu6b34fw" target="_blank" rel="noreferrer"><Button>Тест "Блок регенерации МЭА"</Button></a>
-                <a href="https://onlinetestpad.com/5yuhaqbv6pkpq" target="_blank" rel="noreferrer"><Button>Тест "Блок очистки ВСГ"</Button></a>
+                <ModalComponent
+                  name="Правда или ложь"
+                  href="https://wordwall.net/ru/embed/ae746e14bc5b495297779fb7e866e157?themeId=48&templateId=35%22%20width=%22500%22%20height=%22380%22%20frameborder=%220%22%20allowfullscreen"
+                />
+                <ModalComponent
+                  name="Найди слова"
+                  href="https://wordwall.net/ru/embed/ff041e58354c4dff975a284f728e88bc?themeId=44&templateId=10%22%20width=%22500%22%20height=%22380%22%20frameborder=%220%22%20allowfullscreen"
+                />
+                <ModalComponent
+                  name="Назначение блоков"
+                  href="https://learningapps.org/watch?v=p2ts0hr4t21"
+                />
+                <a
+                  href="https://onlinetestpad.com/2wyebagof4os6"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Button>Кроссворд</Button>
+                </a>
+                <a
+                  href="https://onlinetestpad.com/fbptycgep2bby"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Button>Тест "Реакторный блок"</Button>
+                </a>
+                <a
+                  href="https://onlinetestpad.com/5yuhaqbv6pkpq"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Button>Тест "Блок стабилизации"</Button>
+                </a>
+                <a
+                  href="https://onlinetestpad.com/icdxqfu6b34fw"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Button>Тест "Блок регенерации МЭА"</Button>
+                </a>
+                <a
+                  href="https://onlinetestpad.com/5yuhaqbv6pkpq"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Button>Тест "Блок очистки ВСГ"</Button>
+                </a>
               </div>
             </AccordionPanel>
           </AccordionItem>
@@ -248,6 +294,8 @@ function Hydrotreating() {
           </AccordionItem>
         </Accordion>
       </div>
+      <HydrotreatingSwiper />
+
     </div>
   );
 }
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,33 +1,30 @@
-import React from 'react'
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import Tatorey from './Tatorey'
-import VT1 from '../components/VT-1'
-import Hydrotreating from './Hydrotreating'
+import React from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import Tatorey from "./Tatorey";
+import VT1 from "../components/VT-1";
+import Hydrotreating from "./Hydrotreating";
 
 function tabs() {
   return (
-
-    <Tabs isFitted variant='enclosed'>
-      <TabList mb='1em'>
+    <Tabs isFitted variant="enclosed">
+      <TabList mb="1em">
         <Tab>Гидроочистка</Tab>
         <Tab>ВТ-1</Tab>
         <Tab>Таторей</Tab>
       </TabList>
-      <TabPanels>
-        <TabPanel>
-          <Tatorey />
-
-        </TabPanel>
-        <TabPanel>
-          <VT1 />
-
-        </TabPanel>
-        <TabPanel>
+      <TabPanels >
+        <TabPanel style={{padding: '0'}}>
           <Hydrotreating />
+        </TabPanel>
+        <TabPanel style={{padding: '0'}}>
+          <VT1 />
+        </TabPanel>
+        <TabPanel style={{padding: '0'}}>
+          <Tatorey />
         </TabPanel>
       </TabPanels>
     </Tabs>
-  )
+  );
 }
 
-export default tabs
+export default tabs;
